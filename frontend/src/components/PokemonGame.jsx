ec260c086e2101b70d1d0c47143774223a4276f5
Jagholin <pavel.belskiy2@gmail.com>
2023-02-01 14:48:00 +0100

    register yourself on first coming to the Game page add heartbeat only show players that were recently online add MUI interface elements

--- frontend/src/components/PokemonGame.jsx
+++ frontend/src/components/PokemonGame.jsx
@@ -1,19 +1,52 @@
-import React from 'react'
-import { lobbyArray } from '../pocketbase/pb';
+import React, { useState } from 'react'
+import { lobbyArray, addMyself } from '../pocketbase/pb';
 import { useAtomValue } from 'jotai';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, TextField, Typography, Button } from '@mui/material';
 
 export default function PokemonGame() {
   const lobby = useAtomValue(lobbyArray);
-  console.log("New lobby in the PokemonGame: ", lobby)
+  const [userDialogOpened, setUserDialogOpened] = useState(true);
+  const [userName, setUserName] = useState("");
+
+  const handleRegisterUser = () => {
+    setUserDialogOpened(false);
+    addMyself(userName, 'available');
+  }
 
   return (
-    <div>
-      This is game's lobby!
-      <ul>
-        {lobby.map(player => (
-          <li key={player.id}>{player.name}: {player.status}, updated on {player.updated}. ID {player.id}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Grid container gap={2}>
+        <Grid item xs={12} >
+          <Typography variant='body1'>
+            This is game's lobby!
+          </Typography>
+        </Grid>
+        <Grid item xs={6} >
+          <ul>
+            {lobby
+              .filter(player => Date.now() - player.updated < 10000)
+              .map(player => (
+              <li key={player.id}>{player.name}: {player.status}, updated {(Date.now() - player.updated) / 1000}s ago. ID {player.id}</li>
+            ))}
+          </ul>
+        </Grid>
+        <Grid item xs={6}>
+
+        </Grid>
+      </Grid>
+      <Dialog open={userDialogOpened}>
+        <DialogTitle>Enter your name</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Enter your name to register in the game lobby:
+          </DialogContentText>
+          <TextField autoFocus label='Name' fullWidth variant='standard' value={userName} onChange={(e) => setUserName(e.target.value)}/>
+        </DialogContent>
+        <DialogActions>
+          <Button>Back</Button>
+          <Button onClick={handleRegisterUser}>Register</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
--- frontend/src/pocketbase/pb.js
+++ frontend/src/pocketbase/pb.js
@@ -9,7 +9,11 @@ const lobby = pbClient.collection("lobby");
 // synchronize our information at the start of the app lifetime
 
 export const lobbyArray = atom([]);
+export const myself = atom(null);
 const jotaiStore = getDefaultStore();
+
+const heartbeatTimer = null;
+let lastBeatTimestamp = 0;
 
 const items = await lobby.getFullList();
 
@@ -27,8 +31,8 @@ lobby.subscribe("*", (data) => {
             id: data.record.id,
             name: data.record.name,
             status: data.record.status,
-            created: data.record.created,
-            updated: data.record.updated,
+            created: new Date(data.record.created),
+            updated: new Date(data.record.updated),
         }])
     } else if (data.action === "update") {
         console.log("Witness object update: ", data.record);
@@ -36,13 +40,59 @@ lobby.subscribe("*", (data) => {
             id: data.record.id,
             name: data.record.name,
             status: data.record.status,
-            created: data.record.created,
-            updated: data.record.updated,
+            created: new Date(data.record.created),
+            updated: new Date(data.record.updated),
         } : oldItem));
+        if (jotaiStore.get(myself)?.id === data.record.id) {
+            console.log("Detected changes to myself");
+            console.log("milliseconds for round-trip:", Date.now() - lastBeatTimestamp);
+            jotaiStore.set(myself, {...data.record});
+        }
     } else if (data.action === "delete") {
         console.log("Witness object deletion: ", data.record);
         jotaiStore.set(lobbyArray, jotaiStore.get(lobbyArray).filter(oldItem => oldItem.id !== data.record.id));
+        if (jotaiStore.get(myself).id === data.record.id) {
+            console.log("Detected changes to myself");
+            if (heartbeatTimer) {
+                clearInterval(heartbeatTimer);
+                heartbeatTimer = null;
+            }
+            jotaiStore.set(myself, null);
+        }
     } else {
         console.error("Unexpected event received in lobby.subscribe: ", data);
     }
 })
+
+function heartbeat(id) {
+    return async () => {
+        try {
+            const me = jotaiStore.get(myself);
+            lastBeatTimestamp = Date.now();
+            const res = await lobby.update(id, {}); // dont know if it works this way: YES IT FREAKIN DOES
+        } catch (err) {
+            console.log("heartbeat totally errors out: ", err);
+        }
+    }
+}
+
+export async function addMyself(name, status) {
+    const currentLobby = jotaiStore.get(lobbyArray);
+    const foundId = currentLobby.find(val => val.name === name);
+    let retObj;
+    if (foundId) {
+        // we have found a user with the given name in the table.
+        // now we have to just update it
+        retObj = await lobby.update(foundId.id, {
+            name, status
+        });
+        // the atom will be updated by the realtime listener, so we don't need to do it here.
+    } else {
+        retObj = await lobby.create({
+            name, status
+        });
+        // the atom will be updated by the realtime listener, so we don't need to do it here.
+    }
+    jotaiStore.set(myself, retObj);
+    setInterval(heartbeat(retObj.id), 4000);
+}
